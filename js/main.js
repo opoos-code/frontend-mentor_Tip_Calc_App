35e07e0cdbc520203d9ea78be3842b4a4bf099bd
opoos-code <oliverpoos@t-online.de>
2021-11-11 18:45:36 +0100

    Initial commit

---
+++ js/main.js
@@ -0,0 +1,86 @@
+var tipBtns = document.getElementsByClassName('btn')
+console.log(tipBtns)
+for (var i = 0; i < tipBtns.length; i++){
+    var Btn = tipBtns[i];
+    Btn.addEventListener('click', function(event) {
+        click(event.target)
+    });
+}
+var inputBill = document.getElementById('bill');
+inputBill.addEventListener('change', function() {
+    var tipSelected = document.getElementsByClassName('btn-active')[0].textContent;
+    updateTip(tipSelected);
+    updateTotal();
+
+});
+var inputNumofPeople = document.getElementById('numofpeople');
+inputNumofPeople.addEventListener('change', function() {
+    var tipSelected = document.getElementsByClassName('btn-active')[0].textContent;
+    document.getElementsByClassName("Error-Message")[0].classList.add("hide");
+    document.getElementById("numofpeople").classList.remove("Error");
+    updateTip(tipSelected);
+    updateTotal();
+})
+
+
+function updateTip(tipSelected) {
+    
+    var tipValue = parseInt(tipSelected);
+    var numberOfPeople = document.getElementById('numofpeople').value;
+
+    var bill = document.getElementById('bill').value
+    console.log(tipValue/100 * bill/numberOfPeople);
+    var tip = tipValue/100 * bill/numberOfPeople;
+    var tip = Math.round(tip*100)/100;
+    document.getElementById('tip').textContent = '$'+tip;
+}
+
+function updateTotal() {
+
+    var numberOfPeople = document.getElementById('numofpeople').value;
+    var bill = document.getElementById('bill').value;
+    var tippP = document.getElementById('tip').textContent.substring(1);
+    var tippPNum = parseFloat(tippP);
+
+    console.log(bill / numberOfPeople + tippPNum);
+    var total = bill / numberOfPeople + tippPNum;
+    total = Math.round(total*100) / 100;
+    document.getElementById('total').textContent = '$'+total;
+}
+
+function click(target) {
+    
+    var activeBtn = document.getElementsByClassName('btn-active')[0];
+    var numberOfPeople = document.getElementById('numofpeople').value;
+    if (numberOfPeople > 0)
+    { 
+        console.log(activeBtn);
+        console.log(target);
+        if (activeBtn) {
+            activeBtn.classList.remove("btn-active");
+        }
+        if (target == document.getElementById("custom")) {
+            target.outerHTML = `<input class='btn-input' type='number'>`
+            document.getElementsByClassName("btn-input")[0].addEventListener('change', function(event){
+                let tipp = event.target.value
+                event.target.outerHTML = `<button class='btn btn-active' id="custom" type="button">${tipp}%</button>`
+                document.getElementById("custom").addEventListener('click', function(event){
+                    click(event.target);
+                })
+                updateTip(tipp);
+                updateTotal();
+            });
+        } else {
+        var tipSelected = target.textContent;
+        target.classList.add("btn-active");
+        console.log(tipSelected);
+        updateTip(tipSelected);
+        updateTotal();
+        }   
+    } else {
+        console.log("NumofPeople smaller than 1")
+        document.getElementById("numofpeople").classList.add("Error");
+        document.getElementsByClassName("Error-Message")[0].classList.remove("hide");
+    }
+
+}
